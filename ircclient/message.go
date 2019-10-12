f3639645e246ed3655b9bb60eec1c460d073f26e
l3uddz <l3uddz@gmail.com>
2019-10-12 18:23:13 +0100

    Only process message from known channels / announcers

--- ircclient/message.go
+++ ircclient/message.go
@@ -1,14 +1,30 @@
 package ircclient
 
-import irc "github.com/thoj/go-ircevent"
+import (
+	listutils "github.com/l3uddz/trackarr/utils/lists"
+	irc "github.com/thoj/go-ircevent"
+)
 
 /* Private */
 
 func (c *IRCClient) handleMessage(event *irc.Event) {
+	// ignore messages if not from an known channel / announcer
 	channelName := event.Arguments[0]
-	cleanMessage := c.cleanMessage(event.Message())
 
+	if !listutils.StringListContains(c.parser.Tracker.Channels, channelName, false) {
+		log.Tracef("Ignoring message from non-allowed channel %s -> %s", channelName, event.Message())
+		return
+	} else if !listutils.StringListContains(c.parser.Tracker.Announcers, event.User, false) {
+		log.Tracef("Ignoring message from non-allowed announcer %s -> %s", event.User, event.Message())
+		return
+	}
+
+	// clean message
+	cleanMessage := c.cleanMessage(event.Message())
 	c.log.Tracef("%s -> %s", channelName, cleanMessage)
+
+	// process message
+
 }
 
 func (c IRCClient) cleanMessage(message string) string {
