ecd10b3c36b3531aa6a5c761fa0f83a438f46d3d
l3uddz <l3uddz@gmail.com>
2019-10-25 08:10:42 +0100

    Tasks: fix time logic on pruner task

--- tasks/pruner.go
+++ tasks/pruner.go
@@ -15,7 +15,7 @@ func taskDatabasePruner() {
 	log.Debugf("Database: Pruning releases from database...")
 
 	// remove releases from database that are older than 3 days
-	oldestDate := time.Now().UTC().Truncate(time.Duration(72) * time.Hour)
+	oldestDate := time.Now().UTC().Add(-time.Duration(72) * time.Hour)
 	rowsCleared := database.DB.Unscoped().Delete(models.PushedRelease{},
 		"created_at < ?", oldestDate).RowsAffected
 
