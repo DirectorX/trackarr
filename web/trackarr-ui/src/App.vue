0ed374850d429c3ceb6c0711f600783575f661d9
davemaster223 <16483722+daghaian@users.noreply.github.com>
2019-10-29 17:37:51 -0700

    Updated Side Navigation Bar

--- web/trackarr-ui/src/App.vue
+++ web/trackarr-ui/src/App.vue
@@ -1,53 +1,46 @@
 <template>
   <v-app>
-    <v-navigation-drawer
-      fixed
-      permanent
-      width="220px"
-    >
-    
-      <v-list
-        nav
-        dense
-      >
-        <v-list-item-title class="title">
-            Trackarr
-          </v-list-item-title>
+    <v-navigation-drawer app mini-variant permanent>
+      <v-list dense nav>
+        <v-list-item-title class="pa-3 text-center text-uppercase">
+          <span class="font-weight-black">T</span>
+        </v-list-item-title>
         <v-divider></v-divider>
         <v-list-item link>
           <v-list-item-icon>
             <v-icon>mdi-home</v-icon>
           </v-list-item-icon>
-          <v-list-item-title>Home</v-list-item-title>
+          <v-list-item-content>
+          </v-list-item-content>
+       
         </v-list-item>
         <v-list-item link>
           <v-list-item-icon>
             <v-icon>mdi-folder</v-icon>
           </v-list-item-icon>
-          <v-list-item-title>Logs</v-list-item-title>
+          <v-list-item-content>
+          </v-list-item-content>
         </v-list-item>
         <v-list-item link>
           <v-list-item-icon>
             <v-icon>mdi-cogs</v-icon>
           </v-list-item-icon>
-          <v-list-item-title>Settings</v-list-item-title>
+          <v-list-item-content>
+          </v-list-item-content>
         </v-list-item>
       </v-list>
     </v-navigation-drawer>
-    
     <v-content>
-    <router-view/>
+      <router-view />
     </v-content>
   </v-app>
 </template>
 
 <script>
-
-
-export default {
-  name: 'App',
-  data: () => ({
-    //
-  }),
-};
+  export default {
+    name: 'App',
+    data: () => ({
+      //
+    }),
+  };
 </script>
