6a5fe22a4de7b2689fb74cfd026a77bd2162496c
Filipe <filipe@not.sh>
2019-11-03 13:37:49 +1300

    Allow HTTP requests to be retried (#6)
    * Allow HTTP requests to be retried
    
    * Close HTTP body
    
    * Retry on timeout

--- utils/web/request.go
+++ utils/web/request.go
@@ -1,22 +1,43 @@
 package web
 
 import (
-	"github.com/imroc/req"
-	"github.com/pkg/errors"
+	"io/ioutil"
+	"net"
 	"net/http"
+	"os"
 	"time"
 
+	"github.com/imroc/req"
+	"github.com/jpillora/backoff"
+	"github.com/pkg/errors"
+
 	"github.com/l3uddz/trackarr/logger"
+	"github.com/l3uddz/trackarr/utils/lists"
 )
 
 var (
+	// Logging
 	log = logger.GetLogger("web")
+
+	// HTTP client
+	httpClient = &http.Client{
+		Transport: &http.Transport{
+			Dial: (&net.Dialer{
+				Timeout: 10 * time.Second,
+			}).Dial,
+		},
+	}
 )
 
 /* Structs */
 
 // HTTPMethod - The HTTP request method to use
 type HTTPMethod int
+type Retry struct {
+	*backoff.Backoff
+	MaxAttempts          float64
+	RetryableStatusCodes []int
+}
 
 const (
 	// GET - Use GET HTTP method
@@ -32,36 +53,75 @@ const (
 /* Public */
 
 func GetResponse(method HTTPMethod, requestUrl string, timeout int, v ...interface{}) (*req.Resp, error) {
-	var resp *req.Resp
-	var err error
-
 	// prepare request
-	client := &http.Client{Timeout: time.Duration(timeout) * time.Second}
+	client := httpClient
+	client.Timeout = time.Duration(timeout) * time.Second
+
+	req.SetJSONEscapeHTML(false)
 
 	inputs := make([]interface{}, 0)
 	inputs = append(inputs, client)
 	inputs = append(inputs, v...)
 
-	req.SetJSONEscapeHTML(false)
-
-	// send request
-	switch method {
-	case GET:
-		resp, err = req.Get(requestUrl, inputs...)
-	case POST:
-		resp, err = req.Post(requestUrl, inputs...)
-	default:
-		log.Error("Request method has not been implemented")
-		return nil, errors.New("request method has not been implemented")
+	var retry Retry
+	for _, vv := range v {
+		switch vT := vv.(type) {
+		case Retry:
+			retry = vT
+		}
 	}
 
-	// validate response
-	if err != nil {
-		log.WithError(err).Errorf("Failed requesting url: %q", requestUrl)
-		return nil, err
-	} else {
+	// Response var
+	var resp *req.Resp
+	// Exponential backoff
+	for {
+		var err error
+		switch method {
+		case GET:
+			resp, err = req.Get(requestUrl, inputs)
+		case POST:
+			resp, err = req.Post(requestUrl, inputs)
+		default:
+			log.Error("Request method has not been implemented")
+
+			return nil, errors.New("request method has not been implemented")
+		}
+
+		// validate response
+		if err != nil {
+			log.WithError(err).Errorf("Failed requesting url: %q", requestUrl)
+
+			if os.IsTimeout(err) {
+				if retry.MaxAttempts == 0 || retry.Attempt() > retry.MaxAttempts {
+					return nil, err
+				}
+
+				d := retry.Duration()
+				log.Debugf("Retrying failed HTTP request in %s: %d - %q", d, resp.Response().StatusCode, requestUrl)
+
+				time.Sleep(d)
+				continue
+			}
+
+			return nil, err
+		}
+
 		log.Tracef("Request URL: %s", resp.Request().URL)
 		log.Tracef("Request Response: %s", resp.Response().Status)
+
+		if retry.MaxAttempts == 0 || retry.Attempt() > retry.MaxAttempts {
+			break
+		}
+
+		if lists.IntListContains(resp.Response().StatusCode, retry.RetryableStatusCodes) {
+			d := retry.Duration()
+			log.Debugf("Retrying failed HTTP request in %s: %d - %q", d, resp.Response().StatusCode, requestUrl)
+
+			time.Sleep(d)
+			continue
+		}
+
+		break
 	}
 
 	return resp, nil
@@ -73,15 +133,20 @@ func GetBodyBytes(method HTTPMethod, requestUrl string, timeout int, v ...interf
 	if err != nil {
 		return nil, err
 	}
+	defer func() {
+		if err := resp.Response().Body.Close(); err != nil {
+			log.WithError(err).Errorf("Failed to close HTTP response body for url: %q", requestUrl)
+		}
+	}()
 
 	// process response
-	bodyBytes, err := resp.ToBytes()
+	body, err := ioutil.ReadAll(resp.Response().Body)
 	if err != nil {
 		log.WithError(err).Errorf("Failed reading response body for url: %q", requestUrl)
 		return nil, errors.Wrap(err, "failed reading url response body")
 	}
 
-	return bodyBytes, nil
+	return body, nil
 }
 
 func GetBodyString(method HTTPMethod, requestUrl string, timeout int, v ...interface{}) (string, error) {
