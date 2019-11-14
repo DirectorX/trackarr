e9a1339b979a27105dbca1ad9607f2cedb8ea41e
davemaster223 <16483722+daghaian@users.noreply.github.com>
2019-11-14 14:21:34 -0800

    [Web] Add lazy loading of components

--- web/trackarr-ui/src/router/index.js
+++ web/trackarr-ui/src/router/index.js
@@ -1,8 +1,8 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
-import Logs from '../views/Logs.vue'
-import Status from '../views/Status.vue'
+const Home  = () => import('../views/Home.vue')
+const Logs = () => import('../views/Logs.vue')
+const Status = () => import('../views/Status.vue')
 
 Vue.use(VueRouter)
 
