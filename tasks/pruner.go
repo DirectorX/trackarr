3408c9a3f8c31f3a42256fa8bb02175e84a2c3a9
l3uddz <l3uddz@gmail.com>
2019-11-22 19:48:58 +0000

    Pruner: Customize max age before releases start being removed from database

--- tasks/pruner.go
+++ tasks/pruner.go
@@ -2,6 +2,7 @@ package tasks
 
 import (
 	"fmt"
+	"github.com/l3uddz/trackarr/config"
 	"time"
 
 	"github.com/l3uddz/trackarr/database"
@@ -23,7 +24,7 @@ func taskDatabasePruner() {
 	log.Debug("Database: Pruning releases from database...")
 
 	// find releases older than X days
-	oldestDate := time.Now().UTC().Add(-time.Duration(72) * time.Hour)
+	oldestDate := time.Now().UTC().Add(-time.Duration(config.Config.Database.MaxAgeHours) * time.Hour)
 
 	query := database.DB.Select(q.Lte("CreatedAt", oldestDate))
 	if err := query.Find(&releases); err != nil && err != storm.ErrNotFound {
