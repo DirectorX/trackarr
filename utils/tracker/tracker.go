177ee5972d24eff62aa0a4c9cc6ab111b73554ff
l3uddz <l3uddz@gmail.com>
2020-04-03 21:21:45 +0100

    change(tracker): use actual tracker name for log field

--- utils/tracker/tracker.go
+++ utils/tracker/tracker.go
@@ -29,7 +29,7 @@ func GetApi(tracker *config.TrackerInstance) (Interface, error) {
 	switch strings.ToLower(tracker.Name) {
 	case "passthepopcorn":
 		return &Ptp{
-			log:     log.WithField("api", "ptp"),
+			log:     log.WithField("api", tracker.Name),
 			tracker: tracker,
 		}, nil
 
