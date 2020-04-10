c420491fc0f70ddb0a1159f97948ff8d011da78d
l3uddz <l3uddz@gmail.com>
2020-04-10 21:18:48 +0100

    change(irc): dont process invite callbacks when no channel has been determined

--- ircclient/invite.go
+++ ircclient/invite.go
@@ -9,10 +9,13 @@ import (
 /* Private */
 
 func (c *IRCClient) handleInvite(event *irc.Event) {
-	// parse invite channel
+	// parse channel invited too
 	channel := "Unknown"
 	if len(event.Arguments) >= 2 && len(event.Arguments[1]) >= 2 && event.Arguments[1][0] == '#' {
 		channel = event.Arguments[1]
+	} else {
+		// we should always know what channel we have been invited too
+		return
 	}
 
 	// validate invite to an expected announce channel
@@ -21,7 +24,7 @@ func (c *IRCClient) handleInvite(event *irc.Event) {
 		return
 	}
 
-	// is invite from an expected announcer ?
+	// validate invite from an expected announcer
 	if !lists.StringListContains(c.Tracker.Info.Announcers, event.Nick, false) {
 		c.log.Warnf("Ignoring invite to %q by: %q", channel, event.Nick)
 		return
